85e0c2cd7c2b9275acac42e95cddee47db16c5f0
jlsix <44803642+jlsix@users.noreply.github.com>
2020-02-04 16:17:25 +0100

    Update env-setup.sh

--- env-setup.sh
+++ env-setup.sh
@@ -1,2 +1,2 @@
 sudo /usr/bin/anaconda/bin/conda create --prefix /usr/bin/anaconda/envs/env1 python=3.5 anaconda --yes
-sudo /usr/bin/anaconda/bin/conda install numpy -n evn1 --yes
+sudo /usr/bin/anaconda/bin/conda install numpy -n env1 --yes
